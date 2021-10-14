3637d909f107dac468b3ee6e088e70619e38bc42
RozeFound <RozeFound@gmail.com>
2021-10-14 22:32:42 +0700

    Cleaned up main, and added config support

--- mc_converter/Helpers/utils.py
+++ mc_converter/Helpers/utils.py
@@ -1,5 +1,7 @@
 from ctypes import ArgumentError
+from os import walk
 from pathlib import Path
+from typing import Any
 
 def get_hash(path: Path, type: str = "sha256") -> str | int:
         
@@ -22,3 +24,62 @@ def get_hash(path: Path, type: str = "sha256") -> str | int:
         case _: raise(ArgumentError("Incorrect hash type!"))
 
     return hash
+
+def get_pack_format(path: Path) -> str:
+
+    # Key modpack identifiers:
+    # Modrinth has index.json
+    # MultiMC has mmc-pack.json and instance.cfg
+    # CurseForge has manifest.json and modlist.html
+    # packwiz is a folder, and has index.toml and pack.toml
+    # So.. That was easier then I thought
+
+    if path.is_dir():
+        files = [file for _, _, filenames in walk(path) for file in filenames]
+        if "index.toml" in files and "pack.toml" in files: return "packwiz"
+
+    if str(path).endswith("zip"):
+        from zipfile import ZipFile
+        with ZipFile(path) as zip:
+
+            filenames = [Path(file).name for file in zip.namelist()]
+
+            if "index.json" in filenames: return "modrinth"
+            elif "mmc-pack.json" in filenames and "instance.cfg" in filenames: return "multimc"
+            elif "manifest.json" in filenames and "modlist.html" in filenames: return "curseforge"
+
+    return "Unknown"
+
+def get_default_config() -> dict[str, Any]:
+
+    config = {
+                "author": str(),
+                "name": str(),
+                "version": str(),
+                "description": str(),
+
+                "minecraft": str(),
+                "modloader": {
+                    "type": str(),
+                    "version": str()
+                },
+
+                "resources": [],
+                "overrides": []
+            }
+
+    return config
+
+from mc_converter.multimc import MultiMCManager
+from mc_converter.curseforge import CurseForgeManager
+from mc_converter.modrinth import ModrinthManager
+from mc_converter.packwiz import packwizManager
+
+def get_pack_manager(pack_format: str):
+
+    match pack_format:
+
+        case "multimc": return MultiMCManager
+        case "curseforge": return CurseForgeManager
+        case "modrinth": return ModrinthManager
+        case "packwiz": return packwizManager
--- mc_converter/main.py
+++ mc_converter/main.py
@@ -1,43 +1,44 @@
 from argparse import ArgumentParser
+from aiohttp import ClientSession
 from pathlib import Path
-from os import walk
 
-def get_pack_format(path: Path) -> str:
+from json import dump as write_json
+from tomli import load as parse_toml
 
-    # Key modpack identifiers:
-    # Modrinth has index.json
-    # MultiMC has mmc-pack.json and instance.cfg
-    # CurseForge has manifest.json and modlist.html
-    # packwiz is a folder, and has index.toml and pack.toml
-    # So.. That was easier then I thought
-
-    if path.is_dir():
-        files = [file for _, _, filenames in walk(path) for file in filenames]
-        if "index.toml" in files and "pack.toml" in files: return "packwiz"
-
-    if str(path).endswith("zip"):
-        from zipfile import ZipFile
-        with ZipFile(path) as zip:
-
-            filenames = [Path(file).name for file in zip.namelist()]
-
-            if "index.json" in filenames: return "modrinth"
-            elif "mmc-pack.json" in filenames and "instance.cfg" in filenames: return "multimc"
-            elif "manifest.json" in filenames and "modlist.html" in filenames: return "curseforge"
-
-    return "Unknown"
+from .Helpers.utils import get_pack_format, get_default_config, get_pack_manager
 
 async def main():
 
-    formats = ['packwiz', 'multimc', 'modrinth', 'curseforge', 'intermediary']
+    formats = ['packwiz', 'multimc', 'modrinth', 'curseforge', 'intermediate']
  
     parser = ArgumentParser(description="Converts modpack formats to each other", exit_on_error=True)
     parser.add_argument('-c', '--config', dest='config', type=Path, help='Path to config, used to fill the gaps in parsed data')
     parser.add_argument('-i', '--input', dest='input', type=Path, help='Path to pack', required=True)
-    parser.add_argument('-f', '--format', dest='format', type=str, choices=formats, help='Format to convert to', required=True)
+    parser.add_argument('-f', '--format', dest='format', type=str, nargs="+", choices=formats, help='Format to convert to', required=True)
     parser.add_argument('-o', '--output', dest='output_dir', type=Path, help='Specify output directory (optional)')
     args = parser.parse_args()
 
     if not args.input.exists(): exit("Invalid input!")
     pack_format = get_pack_format(args.input)
     if pack_format == "Unknown": exit("Invalid pack input format!")
+
+    config = get_default_config()
+
+    if args.config:
+        with open(args.config) as file:
+            config.update(parse_toml(file))
+
+    manager_class = get_pack_manager(pack_format)
+
+    async with ClientSession() as session:
+        input_manager = manager_class(session, config)
+        config = await input_manager.parse(args.input)
+
+        if not config['author']: config['author'] = input("Author: ")
+        if not config['name']: config['name'] = input("Name: ")
+        if not config['version']: config['version'] = input("Version: ")
+        if not config['description']: config['description'] = input("Description: ")
+
+    if "intermediate" in args.format:
+        with open("intermediate_outout.json", "w") as file:
+            write_json(config, file, indent=4)
