f0df3390f0a0e695f29fa0995039b14fa814a6b4
RozeFound <rozefound@gmail.com>
2022-09-02 20:40:46 +0700

    Respect XDG variables

--- mmc_export/config.py
+++ mmc_export/config.py
@@ -1,10 +1,14 @@
 from pathlib import Path
 from cryptography.fernet import Fernet
+from os import environ
 
 _token = b'gAAAAABifAIMNFaSNF8epJIDWIv2nSe3zxARkMmViCa1ZCvtwoRqhuB1LYjjJsAstwTvP4dEOSm6Wj0SRDWr3PPwZz5eEBt_1fU8uIaninakGYFNSarEduD6YfoA-rm28qUQHYpVcuae3lj8sYrs_87P6F4s3gBrYg=='
 _key = b'ywE5qRot_nuWfLnbEXXcAPKaW10us3YpWEkDXgm9was='
 
 CURSEFORGE_API_TOKEN = Fernet(_key).decrypt(_token).decode()
-DEFAULT_CACHE_DIR = Path().home() / ".cache/mmc-export"
 OAUTH_GITHUB_CLIENT_ID = "8011f22f502b091464de"
+
+CACHE_HOME = environ.get("XDG_CACHE_HOME", Path().home() / ".cache")
+DEFAULT_CACHE_DIR = Path(CACHE_HOME) / "mmc-export"
+
 output_naming_scheme = "{abbr}_{name}"
